e4ba75233b06957196befd3cb7522df246a3f298
unknown <arjunmahesh97@gmail.com>
2020-04-28 02:05:06 +0530

    Door

--- Source/Escape/WorldPosition.cpp
+++ Source/Escape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,7 +20,11 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString objectName;
+
+	objectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Object name is : %s"), *objectName);
 	
 }
 
